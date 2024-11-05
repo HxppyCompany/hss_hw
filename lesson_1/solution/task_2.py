59a83351b26c2279edebf62e1ec9b36ba734a6ad
HxppyFan <hapfan@internet.ru>
2024-11-05 18:30:07 +0400

    task 2-5 completed

---
+++ lesson_1/solution/task_2.py
@@ -0,0 +1,34 @@
+"""Word game"""
+import string
+from pprint import pprint
+
+
+def main():
+    words = [w for w in input().strip().split()]
+
+    for i in range(len(words)):
+        for ch in string.punctuation:
+            words[i] = words[i].replace(ch, '')
+
+    res = check(words)
+    pprint(res)
+
+
+def check(words):
+    incorrect = list()
+    used = [words[0]]
+
+    for i in range(1, len(words)):
+        if not used[-1].endswith(words[i][0]):
+            incorrect.append(i + 1)
+        else:
+            if words[i] not in used:
+                used.append(words[i])
+            else:
+                incorrect.append(i + 1)
+
+    return incorrect
+
+
+if __name__ == "__main__":
+    main()
